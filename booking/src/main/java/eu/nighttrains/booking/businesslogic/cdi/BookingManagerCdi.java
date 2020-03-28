a88490c0d49a08400755d38db0cc9bc65b24956b
Alexander Doppelbauer <S1910454005@students.fh-hagenberg.at>
2020-03-28 20:05:18 +0100

    fix booking support for different traincar types

--- booking/src/main/java/eu/nighttrains/booking/businesslogic/cdi/BookingManagerCdi.java
+++ booking/src/main/java/eu/nighttrains/booking/businesslogic/cdi/BookingManagerCdi.java
@@ -96,7 +96,7 @@ public class BookingManagerCdi implements BookingManager {
         List<List<RailwayStationConnection>> ticketConnections = separator
                 .separateConnections(connections);
 
-        Booking booking = book(ticketConnections);
+        Booking booking = book(bookingRequest, ticketConnections);
         booking.setOriginId(originId);
         booking.setDestinationId(destinationId);
         Booking savedBooking = saveBooking(booking);
@@ -115,25 +115,25 @@ public class BookingManagerCdi implements BookingManager {
         return mergedBooking;
     }
 
-    public Booking book(List<List<RailwayStationConnection>> ticketConnections){
+    public Booking book(BookingRequestDto2 bookingRequest, List<List<RailwayStationConnection>> ticketConnections){
         Booking booking = new Booking();
         List<Ticket> ticketList = new ArrayList<>();
         for(List<RailwayStationConnection> connections : ticketConnections){
-            Ticket ticket = createTicket(connections);
+            Ticket ticket = createTicket(bookingRequest, connections);
             ticketList.add(ticket);
         }
         booking.setTickets(ticketList);
         return booking;
     }
 
-    private Ticket createTicket(List<RailwayStationConnection> connections) {
+    private Ticket createTicket(BookingRequestDto2 bookingRequest, List<RailwayStationConnection> connections) {
         Ticket ticket = new Ticket();
         List<Reservation> reservationList = new ArrayList<>();
         for(RailwayStationConnection connection : connections){
             if(ticket.getOriginId() == -1){
                 ticket.setOriginId(connection.getDepartureStation().getId());
             }
-            Reservation reservation = makeReservation(connection);
+            Reservation reservation = makeReservation(bookingRequest, connection);
             reservationList.add(reservation);
             ticket.setDestinationId(connection.getArrivalStation().getId());
         }
@@ -141,8 +141,9 @@ public class BookingManagerCdi implements BookingManager {
         return ticket;
     }
 
-    private Reservation makeReservation(RailwayStationConnection connection) {
-        TrainCar trainCar = findAvailableTrainCar(TrainCarType.SLEEPER, connection);
+    private Reservation makeReservation(BookingRequestDto2 bookingRequest, RailwayStationConnection connection) {
+        TrainCarType trainCarType = bookingRequest.getTrainCarType();
+        TrainCar trainCar = findAvailableTrainCar(trainCarType, connection);
         if(trainCar != null){
             Reservation reservation = createReservation(connection, trainCar, connection.getDate());
             return reservation;
